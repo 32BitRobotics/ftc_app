d39643be3d86c1f5c611017f8c586c540ee518c1
not_a_seagull <notaseagull048@gmail.com>
2019-09-12 15:54:48 -0700

    Added gyro sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpBase.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -8,6 +9,10 @@ import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
 public abstract class OpBase extends LinearOpMode {
 
     // hardware members
@@ -15,13 +20,17 @@ public abstract class OpBase extends LinearOpMode {
     public DcMotor frontRight = null; //front right wheel
     public DcMotor backLeft = null; //back left wheel
     public DcMotor backRight = null; //back right wheel
-    public GyroSensor gyroSensor = null;
+    public BNO055IMU gyroSensor = null;
 
     // Declare OpMode members.
     protected ElapsedTime runtime = new ElapsedTime();
 
     protected abstract boolean runRobot(int goSeconds);
     protected abstract boolean isAutonomous();
+
+    protected double getZAngle() {
+        return gyroSensor.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES).firstAngle;
+    }
 
     @Override
     public void runOpMode() {
@@ -30,6 +39,16 @@ public abstract class OpBase extends LinearOpMode {
         frontRight = hardwareMap.dcMotor.get("frontRight");
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
+
+        // initialize gyro
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+
+        gyroSensor = hardwareMap.get(BNO055IMU.class, "imu");
+        gyroSensor.initialize(parameters);
 
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.FORWARD);
